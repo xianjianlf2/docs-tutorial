c4304c81c7159c00d1ffb5612e9abc568b178b44
Mark Xian <mark-xian@foxmail.com>
2025-10-27 17:51:14 +0800

    feat: integrate Tiptap extensions for enhanced editing capabilities

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,13 +1,38 @@
 'use client'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-
-
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Table from '@tiptap/extension-table'
+import Text from '@tiptap/extension-text'
+import Document from '@tiptap/extension-document'
+import Gapcursor from '@tiptap/extension-gapcursor'
+import Paragraph from '@tiptap/extension-paragraph'
+import Image from '@tiptap/extension-image'
 
 const Editor = () => {
     const editor = useEditor({
-        extensions: [StarterKit],
-        content: '<p>Hello World! 🌎️</p>',
+        extensions: [StarterKit, TaskList, TaskItem.configure({
+            nested: true,
+        }), Document,
+            Paragraph,
+            Text,
+            Gapcursor,
+            Table.configure({
+                resizable: true,
+            }),
+            TableRow,
+            TableHeader,
+            TableCell,
+            Image],
+        content: `
+          <p>This is a basic example of implementing images. Drag to re-order.</p>
+        <img src="https://placehold.co/800x400" />
+        <img src="https://placehold.co/800x400/6A00F5/white" />
+      `,
         editorProps: {
             attributes: {
                 style: 'padding-left: 56px;padding-right:56px;',
