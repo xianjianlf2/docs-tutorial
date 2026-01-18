5b91abe33c4441debbf4514bcdaa3dca91281f16
xianjianlf2 <mark-xian@foxmail.com>
2026-01-18 17:01:04 +0800

    feat: add Tiptap heading extension, refactor authentication configuration, and simplify toast action types in useToast hook

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,10 +1,10 @@
-import { AuthConfig } from "convex/server";
-
-export default {
+const authConfig = {
   providers: [
     {
       domain: 'https://enabling-oryx-95.clerk.accounts.dev',
       applicationID: "convex",
     },
-  ]
-} satisfies AuthConfig;
+  ],
+};
+
+export default authConfig;
